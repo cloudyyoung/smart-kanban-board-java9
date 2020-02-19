4e964167f135848c59d9ed9963a959ca979c710f
Braindamaged69 <60583983+Braindamaged69@users.noreply.github.com>
2020-02-19 14:25:03 -0700

    Update ChildNode.java

--- ChildNode.java
+++ ChildNode.java
@@ -1,17 +1,37 @@
+//this class allows the user to add, remove and edit nodes in the board, collumn
+//or event classes. Ie in the board class a node would be the board itself
+//in the event class the node would be the event etc.
+
 import java.util.ArrayList;
+import java.util.List;
 
 protected ArrayList<Node> nodes = new ArrayList<Node>();
 
 public class ChildNode 
 {
+	//adds a node
 	public Node addNode(Node aNode)
 	{
 		this.nodes.add(aNode);
 	}
 	
-	public Node removeNode(Node)
+	//removes a node
+	public Node removeNode(int id)
 	{
-		//NOT IMPLEMENTED
+		Iterator<Node> itr = nodes.iterator();
+
+        //while there are nodes in the list, check if node is
+		//equal to the parameter, and if so removes that node
+        while (itr.hasNext()) 
+        {
+            Node nodeCheck = itr.next();
+    
+            if (nodeCheck = id)
+            {
+            	nodes.remove(nodeCheck)
+            }
+        }
+		
 	}
 	
 	public Node updateNode(int id, Node aNode)
