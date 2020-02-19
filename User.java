9c4123d2a18e6c6186ee0941dc698923fbc8fc07
Braindamaged69 <60583983+Braindamaged69@users.noreply.github.com>
2020-02-19 12:56:04 -0700

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,57 @@
 
-public class User {
+public class User 
+{
+	private String username;
+	private int id;
+	private String sessionId;
+	private String password;
+	
+	public String getUsername()
+	{
+		return "NOT IMPLEMENTED";
+	}
+	
+	public String getPassword()
+	{
+		return "NOT IMPLEMENTED";
+	}
+	
+	public int getId()
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public String getSessionId()
+	{
+		return "NOT IMPLEMENTED";
+	}
+	
+	public void setUsername(String aUsername)
+	{
+		this.aUsername = username
+	}
+	
+	public void setPassword(String aPassword)
+	{
+		this.aPassword = password
+	}
+	
+	public void setId(int aId)
+	{
+		this.aId = id
+	}
+	
+	public void setSessionId(String aSessionId)
+	{
+		this.aSessionId = sessionId
+	}
+
+	
+	public boolean authenticate(String aUsername, String aPassword)
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	
 
 }
