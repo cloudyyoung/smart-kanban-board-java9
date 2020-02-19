0589f431551b938729cb9aed7c80608263340f59
Braindamaged69 <60583983+Braindamaged69@users.noreply.github.com>
2020-02-19 12:40:44 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,4 +1,73 @@
 
-public class Node {
+public class Node 
+{
+	private int id;
+	private int parent_id;
+	private int grandparent_id;
+	private String title;
+	private String note;
+	private String type;
+	
+	public int getId()
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public int getParentId()
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public int getGrandparentId()
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public void setId(int aId)
+	{
+		this.aId = id;
+	}
+	
+	public void getParentId(int aParentId)
+	{
+		this.aParentId = parent_id;
+	}
+	
+	public void getGrandparentId(int aGrandparentId)
+	{
+		this.aGrandparentId = grandparent_id;
+	}
+	
+	public String getTitle()
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public void setTitle(String aTitle)
+	{
+		this.aTitle = title;
+	}
+	
+	public String getParentType(String aParentType)
+	{
+		//NOT IMPLEMENTED
+
+		return aParentType;
+	}
+	
+	public String getChildType(String aChildType)
+	{
+		//NOT IMPLEMENTED
+
+		return aChildType;
+	}
+	
+	public String toString()
+	{
+		return "NOT IMPLEMETED";
+		
+	}
+	
+	
 
 }
