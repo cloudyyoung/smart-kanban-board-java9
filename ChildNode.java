1965825eae44807bf230a1765ebbf4b94f7c340e
Braindamaged69 <60583983+Braindamaged69@users.noreply.github.com>
2020-02-19 13:34:34 -0700

    Update ChildNode.java

--- ChildNode.java
+++ ChildNode.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
-public class ChildNode {
+protected ArrayList<Node> nodes = new ArrayList<Node>();
+
+public class ChildNode 
+{
+	public Node addNode(Node aNode)
+	{
+		this.nodes.add(aNode);
+	}
+	
+	public Node removeNode(Node)
+	{
+		//NOT IMPLEMENTED
+	}
+	
+	public Node updateNode(int id, Node aNode)
+	{
+		//NOT IMPLEMENTED
+	}
 
 }
