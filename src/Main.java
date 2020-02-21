83f5c190f1b43857e4d787d8647ab8b2c84aedf7
lewisjerremy <lewisjerremy@gmail.com>
2020-02-21 13:37:49 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,7 @@ import structure.Time;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("It's Time");
-    System.out.println(Time.currentHour24());
-    System.out.println(Time.currentHour12(currentHour24()));
-    System.out.println(Time.currentMinute());
-    System.out.println(Time.currentSecond());
-    System.out.println(Time.currentDay());
-    System.out.println(Time.currentDayName());
-    System.out.println(Time.currentMonth());
-    System.out.println(Time.monthName(currentMonth()));
-    System.out.println(Time.currentYear());
 
-    // Terminal.terminal();
+    Terminal.terminal();
   }
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -5,6 +5,7 @@ public class Terminal {
 
   public static void terminal() { // text-based version entrance
     showPage(0);
+
     if (showLogin() == false) {
       showPage(1);
     } else {
@@ -17,11 +18,13 @@ public class Terminal {
   public static boolean showLogin() {
 
     Scanner keyboard = new Scanner(System.in);
+
     boolean authenticated = false;
     boolean showLogin = true;
 
     // If authenticated, pass, otherwise authenticate
     while (authenticated == false) {
+      System.out.println("Please log in.");
       System.out.println("Username: ");
       String username = keyboard.nextLine();
 
@@ -48,35 +51,53 @@ public class Terminal {
       if (command == "help") {
         System.out.println("time : prints the current time");
         System.out.println("exit : terminates the program");
+
       } else if (command == "time") {
-        System.out.println(
-            "Time: "
-                + KanbanTime.currentHour24(formattedDate)
-                + ":"
-                + KanbanTime.currentMinute(formattedDate)
-                + " on "
-                + KanbanTime.currentDayName(myDateObj)
-                + " the "
-                + KanbanTime.currentDay(formattedDate)
-                + " of "
-                + KanbanTime.currentYear(formattedDate));
+        System.out.println("Time: " + Time.currentHour12(Time.currentHour24()) + ":" + Time.currentMinute() + " on "
+            + Time.currentDayName() + " the " + Time.currentDay() + " of " + Time.currentYear());
+
       } else if (command == "exit") {
         onMain = false;
+
       } else {
         System.out.println("Unknown command. Type 'help' for a command list.");
       }
     }
   }
 
-  public static void showSettings() {}
+  public static void showSettings() {
+    Scanner keyboard = new Scanner(System.in);
+    boolean onSettings = true;
 
-  public static void showPage(int page) {
-    if (page == 0) {
-      showLogin();
-    } else if (page == 1) {
-      showMain();
-    } else if (page == 2) {
-      showSettings();
+    System.out.println("You are now in Settings.");
+
+    while (onSettings == true) {
+
+      System.out.println("Enter a cmd: ");
+      String command = keyboard.nextLine();
+
+      if (command == "help") {
+        System.out.println("back : prints the current time");
+        System.out.println("change scedule : terminates the program");
+        System.out.println("clear kanban : prints the current time");
+        System.out.println(" : prints the current time");
+        System.out.println("account settings : terminates the program");
+        System.out.println("exit : terminates the program");
+
+      } else if (command == "time") {
+        System.out.println("Time: " + Time.currentHour12(Time.currentHour24()) + ":" + Time.currentMinute() + " on "
+            + Time.currentDayName() + " the " + Time.currentDay() + " of " + Time.currentYear());
+
+      } else if (command == "back") {
+        showMain();
+        onSettings = false;
+
+      } else if (command == "exit") {
+        onSettings = false;
+
+      } else {
+        System.out.println("Unknown command. Type 'help' for a command list.");
+      }
     }
+
   }
-}
