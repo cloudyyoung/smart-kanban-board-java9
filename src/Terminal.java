2a904fa9eea82621351e9f290672873feedc409e
lewisjerremy <lewisjerremy@gmail.com>
2020-02-21 12:34:13 -0700

    commit

--- src/Terminal.java
+++ src/Terminal.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 public class Terminal {
 
   public static void terminal() { // text-based version entrance
-    int page = 0;
-    showPage(page);
+    showPage(0);
+    if (showLogin() == false) {
+      showPage(1);
+    } else {
+      System.out.println("Please try again.");
+    }
+
+    System.exit(0);
   }
 
   public static void showLogin() {
 
-    boolean authenticated = false;
     Scanner keyboard = new Scanner(System.in);
+    boolean authenticated = false;
+    boolean showLogin = true;
 
     // If authenticated, pass, otherwise authenticate
     while (authenticated == false) {
@@ -21,15 +28,39 @@ public class Terminal {
       System.out.println("Password: ");
       String password = keyboard.nextLine();
 
-      User user = new User();
-      authenticated = user.authenticate(username, password);
+      System.out.println("Welcome " + username + " " + password);
       authenticated = true;
+      showLogin = false;
+    }
+
+    return showLogin;
+  }
+
+  public static void showMain() {
+    Scanner keyboard = new Scanner(System.in);
+    boolean onMain = true;
+    while (onMain == true) {
+      System.out.println("Enter a cmd: ");
+      String command = keyboard.nextLine();
+
+      if (command == "help") {
+        System.out.println("time : prints the current time");
+        System.out.println("exit : terminates the program");
+      } else if (command == "time") {
+        System.out.println("Time: " + KanbanTime.currentHour24(formattedDate) + ":"
+            + KanbanTime.currentMinute(formattedDate) + " on " + KanbanTime.currentDayName(myDateObj) + " the "
+            + KanbanTime.currentDay(formattedDate) + " of " + KanbanTime.currentYear(formattedDate));
+      } else if (command == "exit") {
+        onMain = false;
+      } else {
+        System.out.println("Unknown command. Type 'help' for a command list.");
+      }
+
     }
   }
 
-  public static void showMain() {}
-
-  public static void showSettings() {}
+  public static void showSettings() {
+  }
 
   public static void showPage(int page) {
     if (page == 0) {
