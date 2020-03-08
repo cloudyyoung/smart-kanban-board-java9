9e3be1e583405e99959d7c57036949838a5d5194
calebwannamaker <60583470+calebwannamaker@users.noreply.github.com>
2020-03-08 16:30:03 -0600

    authentication for sign in

--- src/application/loginPageController.java
+++ src/application/loginPageController.java
@@ -1,12 +1,16 @@
-package application; 
+package application;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-
-
-
+import structure.*;
 
 public class loginPageController {
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private TextField passwordField;
 
     @FXML
     private Label signInBtn;
@@ -16,12 +20,20 @@ public class loginPageController {
 
     @FXML
     void handleCreatAcc(MouseEvent e) {
-        
+
     }
 
     @FXML
     void handleSignIn(MouseEvent e) {
-        
+        String username = userNameField.getText();
+        String password = passwordField.getText();
+
+        boolean authenticated = true;
+        User user = new User();
+        HttpRequest req = user.authenticate(username, password);
+        authenticated = req.isSucceed();
+        System.out.println(authenticated);
+
     }
 
 }
