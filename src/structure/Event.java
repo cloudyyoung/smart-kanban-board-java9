4955eaf27867cf997a2544d850e61a65cf8e11c1
jimschenchen <jimschenchen@163.com>
2020-03-30 22:49:25 -0400

    initializing the events constructors

--- src/structure/Event.java
+++ src/structure/Event.java
@@ -1,4 +1,5 @@
 package structure;
+import java.util.*;
 
 /**
  * The {@code Event} class, extends from {@code Node}.
@@ -12,12 +13,34 @@ public class Event extends Node {
    *
    * @param obj the {@code HttpBody} for initialization
    */
-  public Event(HttpBody obj) {
-    super(obj);
-  }
+  int importanceLevel;
+  // importanceLevel range from 0 to 3, which 0 is not important and 3 is super important
+  Long dueDate;
+  // dueDate is the timeStamp
+  Long duration;
+  // Duration store in timeStamp form (millissecond)
 
+  public Event(HttpBody obj, Long dueDate, Long duration, int importanceLevel) {
+    super(obj);
+    this.duration = duration;
+    this.dueDate = dueDate;
+    this.importanceLevel = importanceLevel;
+  }
+ 
   public static void main(String[] args) {
-    System.out.print("CloudyDD");
+    Calendar c = Calendar.getInstance();
+    // c.set(2012, 12, 11, 13, 15);
+    Long dueDate = 12345677123456l;
+    
+    // c.setTimeInMillis(dueDate);
+    // c.set(2012, 0, 11, 13, 15);
+    Long duration = 60000l;
+    c.setTimeInMillis(dueDate);
+    System.out.print(c.getTime());
+
+    c.setTimeInMillis(dueDate + duration);
+    System.out.print(c.getTime());
+    // System.out.print(c.getTimeInMillis());
   }
 }
 
