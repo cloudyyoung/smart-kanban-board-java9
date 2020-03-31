a549c9b93bcf825e4ba1d14db391ab4d5e2d9a53
Cloudy Young <cloudyyoung74@outlook.com>
2020-03-31 07:17:39 -0600

    Updated multiple things

--- src/ui/HomeController.java
+++ src/ui/HomeController.java
@@ -1,63 +1,161 @@
 package ui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.shape.Circle;
+import java.util.HashSet;
+import java.util.Set;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+
+import structure.*;
 
 public class HomeController {
 
-    @FXML
-    private Button sideProfile;
+  @FXML
+  private Button sideProfile;
 
-    @FXML
-    private Circle profileAvatar;
+  @FXML
+  private Circle profileAvatar;
 
-    @FXML
-    private Label profileUsername;
+  @FXML
+  private Label profileUsername;
 
-    @FXML
-    private Button sideSearch;
+  @FXML
+  private VBox sidePane;
 
-    @FXML
-    private Button sideToday;
+  @FXML
+  private Button sideSearch;
 
-    @FXML
-    private Tab boardPane;
+  @FXML
+  private Button sideToday;
 
-    @FXML
-    private TextField boardTitle;
+  @FXML
+  private VBox boardList;
 
-    @FXML
-    private TextField boardNote;
+  @FXML
+  private VBox boardPane;
 
-    @FXML
-    private Button boardAddColumn;
+  @FXML
+  private TextField boardTitle;
 
-    @FXML
-    private HBox columnPane;
+  @FXML
+  private TextField boardNote;
 
-    @FXML
-    private ScrollPane detailPane;
+  @FXML
+  private Button boardAddColumn;
 
-    @FXML
-    private TextArea detailTitle;
+  @FXML
+  private HBox columnPane;
 
-    @FXML
-    private Button detailAdd2Today;
+  @FXML
+  private ScrollPane detailPane;
 
-    @FXML
-    private Button detailDueDate;
+  @FXML
+  private TextArea detailTitle;
 
-    @FXML
-    private Button detailImportance;
+  @FXML
+  private Button detailAdd2Today;
 
-    @FXML
-    private TextField detailNote;
+  @FXML
+  private Button detailDueDate;
+
+  @FXML
+  private Button detailImportance;
+
+  @FXML
+  private TextField detailNote;
+
+  @FXML
+  void initialize() {
+    // Intialize label text values
+    profileUsername.setText(User.current.getUsername());
+    profileUsername.setTooltip(new Tooltip(User.current.getUsername()));
+
+    // Hide detailPane
+    detailPane.getStyleClass().add("hide");
+
+    Kanban.checkout();
+
+    for (structure.Node each : Kanban.current.getChildrenNodes()) {
+      if (each.getId() >= 100) {
+
+        // Icon
+        SVGPath svg = new SVGPath();
+        svg.setContent("M 5 5 L 5 6 L 5 27 L 27 27 L 27 5 L 5 5 z M 7 7 L 25 7 L 25 9 L 7 9 L 7 7 z M 7 11 L 25 11 L 25 25 L 7 25 L 7 11 z");
+        HBox hbox = new HBox();
+        hbox.getChildren().add(svg);
+
+        // Button & text & tooltip
+        Button node = new Button(each.getTitle(), hbox);
+        node.setTooltip(new Tooltip(each.getTitle()));
+        node.setId("board-" + each.getId());
+        node.setStyle(styleAccent(((Board) each).getColor()));
+
+        // Add to list
+        boardList.getChildren().add(node);
+      }else if(each.getId() == 1){
+        sideToday.setStyle(styleAccent(((Board) each).getColor()));
+      }
+    }
+    
+    // Bind button action event for sidePane buttons
+    Set<Node> sideButtons = new HashSet<Node>();
+    sideButtons.addAll(sidePane.lookupAll(".button"));
+    sideButtons.addAll(boardList.lookupAll(".button"));
+
+    for(Node each : sideButtons){
+      System.out.println(each.getId());
+      Button btn = (Button) each;
+      btn.setOnAction(event -> {
+        for (Node eachBtn : sideButtons) {
+          eachBtn.getStyleClass().remove("selected");
+        }
+        Node current = ((Node) event.getSource());
+        current.getStyleClass().add("selected");
+        sideSelectDisplay(current);
+      });
+    }
+
+    sidePane.requestFocus();
+    sideToday.fire();
+  }
+
+  
+  String styleAccent(String hex){
+    String style = "";
+    style += "-fx-accent: " + hex + ";";
+    style += "-fx-accent-90: " + hex + "e6;";
+    style += "-fx-accent-80: " + hex + "cc;";
+    style += "-fx-accent-70: " + hex + "b3;";
+    style += "-fx-accent-60: " + hex + "99;";
+    style += "-fx-accent-50: " + hex + "80;";
+    style += "-fx-accent-40: " + hex + "66;";
+    style += "-fx-accent-30: " + hex + "4d;";
+    style += "-fx-accent-20: " + hex + "33;";
+    style += "-fx-accent-10: " + hex + "1a;";
+    style += "-fx-accent-5: " + hex + "0d;";
+    return style;
+  }
+
+  void sideSelectDisplay(Node btn){
+    String idRaw = btn.getId();
+    if(!idRaw.contains("board-")){
+      return;
+    }
+    String idStr = idRaw.split("-")[1];
+    Integer id = Integer.parseInt(idStr);
+    Board node = (Board) Kanban.current.getNode(id);
+    boardPane.setStyle(styleAccent(node.getColor()));
+
+    boardTitle.setText(node.getTitle());
+    boardNote.setText(!node.getNote().equals("") ? node.getNote() : "No description");
+
+    boardTitle.setDisable(id < 100);
+    boardNote.setDisable(id < 100);
+  }
 
 }
